11ebbd4e8095098ea699b457638483f29f9374af
홍민재 <42972380+98HMJ@users.noreply.github.com>
2023-02-06 14:48:38 +0900

    Update expert.py

--- expert.py
+++ expert.py
@@ -4,158 +4,132 @@ import json
 import requests
 import openpyxl
 from datetime import datetime
+from openpyxl.utils.cell import coordinate_from_string
+
+MAX_HEADER_ROWS_COUNT = 4
+VERIFICATION_TARGET_CELL_INDEX = 0
+DESIGN_VALUE_CELL_INDEX = 3
+NEED_VERIFY_CELL_INDEX = 4
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def hello_world():
     return "hello, world!"
 
-
 @app.route("/expert/review", methods=['POST'])
 def review():
     excel_file = request.files['file']
+
+    # URL 쿼리 파라미터로 검증대상 타입과 대상 partNo 수신.
+    # 검증 대상. ex) "IC", "Transistor", "Diode"
+    target_type = request.args.get('validTarget')
+
+    # 검증 대상 partNo. ex) "LM2576HVSX-ADJ/NOPB"
+    # target_part_no = request.args.get('partNo')
+
     # 엑셀 파일 받아서 파싱
     # 검증 필요한 열들 플랫폼에 검증 요청
-    excel_item,excel_rows = __parse_excel(excel_file)
 
-    # 플랫폼에 최초 1회 호출하는 API
-    # 검증 대상 조회 세션에 문제가 생길 시 최종 response 값으로 {"review start": false} 전달
-    start_check=__review_start_from_platform(excel_item)
-    if start_check['start_check']==False:
-      response = { "review start" : False }
-      return json.dumps(response)
+    header_rows, parsed_rows = __parse_excel(excel_file)
 
-    # 플랫폼에서 검증된 열들의 결과값 저장
+    # DB에 조회할 검증 대상 번호 ex) 'LM2576HVSX-ADJ/NOPB'
+    target_part_no = header_rows[VERIFICATION_TARGET_CELL_INDEX][DESIGN_VALUE_CELL_INDEX]
+
+    # 검증 대상 조회 세션에 문제가 생길 시 최종 response 값으로 {"review_start": false} 전달
+    start_check = __review_start_from_platform(valid_target=target_type, target_part_no=target_part_no)
+
+    # Todo: refactor
+    if not start_check['start_check']:
+        # response = {"review start": False} # key에 공백이 들어가면 안됩니다.
+        response = {"review_start": False}
+        return json.dumps(response)
+
     review_results = []
-    for row in excel_rows:
-        validate_result = __request_part_to_platform(excel_item[0][1],row)
-        review_results.append(validate_result['valid'])
+    for row in parsed_rows:
+        verification_target = row['partName']  # 검증 항목 cell
+        design_value = row['designValue']  # 설계 값 cell
+        validate_result = __request_part_to_platform(part_no=target_part_no, verification_target=verification_target,
+                                                     design_value=design_value)
+        if validate_result['valid'] is not None:
+            review_results.append(validate_result['valid'])
 
+    # Todo: refactor
     # 검증된 열들의 결과값을 종합 처리후 응답
-    passvalidate_results=[]
-    result=True
-    for verification, excel_row in zip(review_results,excel_rows):
-        if verification == False:
-          result=False
-        passvalidate_result={
-            "partName": excel_row[0],
-            "designValue": excel_row[1],
-            "verification": verification,
-        }
-        passvalidate_results.append(passvalidate_result)
-    
+
+    passvalidate_results = []
+    result = True
+    for verification, excel_row in zip(review_results, parsed_rows):
+      if verification == False:
+        result = False
+      passvalidate_result = {
+          "partName": excel_row['partName'],
+          "designValue": excel_row['designValue'],
+          "verification": verification,
+      }
+      passvalidate_results.append(passvalidate_result)
     response = {
-        excel_item[0][0]: excel_item[0][1],
-        excel_item[1][0]: excel_item[1][1],
-        excel_item[2][0]: excel_item[2][1],
-        "passReview": result,
-        "reviewResults": passvalidate_results
+      header_rows[0][VERIFICATION_TARGET_CELL_INDEX]: header_rows[0][DESIGN_VALUE_CELL_INDEX], # "partNo": "LM2576HVSX-ADJ/NOPB"
+      # header_rows[1][0]: header_rows[1][DESIGN_VALUE_CELL_INDEX], # "type": "step_down" ### 엑셀 항목이 비어있어서 추후에 처리
+      header_rows[2][VERIFICATION_TARGET_CELL_INDEX]: header_rows[2][DESIGN_VALUE_CELL_INDEX], # "manufacturer_name": "TI"
+      "passReview": result,
+      "reviewResults": passvalidate_results
     }
-          
-
-    # response = review_results
-    # 응답 예시
-    # return response = {
-    #     "partNo": "LM2576HVSX-ADJ/NOPB",
-    #     "type": "step_down",
-    #     "manufacturer_name": "TI",
-    #     "passReview": false,
-    #     "reviewResults": [
-    #         {
-    #             "partName": "oprating_temperature_min",
-    #             "designValue": -20,
-    #             "verification": true,
-    #         },
-    #         {
-    #             "partName": "oprating_temperature_max",
-    #             "designValue": 100,
-    #             "verification": false,
-    #         },
-    #         {
-    #             "partName": "storage_temperature_min",
-    #             "designValue": -45,
-    #             "verification": true,
-    #         },
-    #         {
-    #             "partName": "storage_temperature_max",
-    #             "designValue": 110,
-    #             "verification": false,
-    #         }
-    #     ]
-    # }
-
     return json.dumps(response)
 
+def __parse_excel(excel_file):  
+    wb = openpyxl.load_workbook(excel_file)
+    sheet_names = wb.sheetnames
+    sheet = wb[sheet_names[0]]
 
-def __parse_excel(excel_file):
-    # 엑셀 파싱후 검증 필요한 rows 반환
-    wb=openpyxl.load_workbook(excel_file)
-    snames=wb.sheetnames
-    sheet=wb[snames[0]]
-    excel_rows=[]
-    item=[]
-    for row in range(1,sheet.max_row):
-      cols=[]
-      for col in sheet.iter_cols(min_col=0, max_col=5):
-          cols.append(col[row].value)
-          del cols[1:3]
-          if row < 4:
-            del cols[2]
-            item.append(cols)
-          else:
-            if cols[2] == 'o':
-              del cols[2]
-              excel_rows.append(cols)
-    return item,excel_rows
+    # Header rows 처리. 검증 항목, 비고, 단위, 설계 값, 적용여부. 1행 한글 row 제외
+    # partNo, type, manufactureer_name. header_rows 넣음
+    header_rows = []
+    header_rows_range = sheet.iter_rows(min_row=2, max_row=MAX_HEADER_ROWS_COUNT, values_only=True)
+    for row in header_rows_range:
+        header_rows.append(row)
 
-def __review_start_from_platform(excel_item):
+    # Header rows 제외
+    # 적용 여부 체크된 row만 parsed_rows 넣음
+    parsed_rows = []
+    rows_range = sheet.iter_rows(min_row=MAX_HEADER_ROWS_COUNT, max_row=sheet.max_row, values_only=True)
+    for row in rows_range:
+        # 빈 row 무시
+        if row is None:
+            continue
 
-    get_url= "http://kws7.kangnam.ac.kr/review/target"
-    data = {'parNo': excel_item[0][1]}
-    validTarget_response=requests.get(get_url, params=data)
-    validTarget=validTarget_response.json()
-  
-    # dockerized url
+        # 적용여부 체크 안 되어있으면 무시
+        if row[NEED_VERIFY_CELL_INDEX] != 'o':
+            continue
+
+        parsed_row = {'partName': row[VERIFICATION_TARGET_CELL_INDEX], 'designValue': row[DESIGN_VALUE_CELL_INDEX]}
+        parsed_rows.append(parsed_row)
+
+    return header_rows, parsed_rows
+
+def __review_start_from_platform(valid_target, target_part_no):
+    data = {'validTarget': valid_target, 'partNo': target_part_no}
     url = "http://platform/review/start"
-
-    # local url
-    # url = "http://localhost:8080/review/start"
     headers = {'Content-Type': 'application/json'}
-    example_body = {
-        "validTarget" : validTarget['validTarget'],
-        "partNo": excel_item[0][1]
-    }
+    body = json.dumps(data)
+    response = json.loads(requests.post(url, data=body, headers=headers).json())
+    return response
 
-    # body = parsed_row
-    body = json.dumps(example_body)
-
-    # response 예시
-    # {'start_check' : true}
-
-    response = requests.post(url, data=body, headers=headers)
-    return json.loads(response.json())
-
-
-def __request_part_to_platform(partNo,parsed_row):
+def __request_part_to_platform(part_no, verification_target, design_value):
     # dockerized url
     url = "http://platform/review/part"
-
-    # local url
-    # url = "http://localhost:8080/review/part"
     headers = {'Content-Type': 'application/json'}
-    example_body = {
-        "partNo": partNo,
-        "verificationTarget": parsed_row[0],
-        "designValue": parsed_row[1]
-    }
+    body = json.dumps({
+        "partNo": part_no,
+        "verificationTarget": verification_target,
+        "designValue": design_value
+    })
 
-    # body = parsed_row
-    body = json.dumps(example_body)
-
-    response = requests.post(url, data=body, headers=headers)
-    return json.loads(response.json())
-
+    # response 예시
+    # {'verificationTarget': 'oprating_temperature_max', 'valid': true'}
+    response = json.loads(requests.post(url, data=body, headers=headers).json())
+    print("__request_part_to_platform API: ", response)
+    return response
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
